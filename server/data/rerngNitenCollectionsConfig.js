1a3703ac2cf385e39f3fbb6b75ca474d0c6eab1a
Vongkakada <vongkakada0@gmail.com>
2025-05-31 17:50:00 +0700

    Update files

--- server/data/rerngNitenCollectionsConfig.js
+++ server/data/rerngNitenCollectionsConfig.js
@@ -1,10 +1,9 @@
-// File: D:\CUS\digital library\imagekit-backend\server\data\rerngNitenCollectionsConfig.js
 
 const collectionsConfig = [
   {
     id: 'a1',
     title: 'ដំណោះស្រាយបច្ចេកវិទ្យាឌីជីថល សម្រាប់លើកកម្ពស់ការអប់រំ បំណិនជីវិតតាមមូលដ្ឋាន',
-    thumbnail: 'https://i.ytimg.com/vi/yxx2-nDxAXc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgZShlMA8=&rs=AOn4CLBGhZFTWSgDcsbQtspDMcfP9s3nng', // Example thumbnail
+    thumbnail: 'https://i.ytimg.com/vi/yxx2-nDxAXc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgZShlMA8=&rs=AOn4CLBGhZFTWSgDcsbQtspDMcfP9s3nng', 
     description: `បង្រៀនដោយ៖ លោកគ្រូ គង់ សុប្ប៉ូលីន
                 បង្រៀននៅ៖ វិទ្យាល័យ ហ៊ុន សែន ពាមជីកង ខេត្តកំពង់ចាម`,
     playlistId: 'PLDt349AvcEaKE6SAspJgOapTu466E-ETf',
@@ -12,34 +11,77 @@ const collectionsConfig = [
   {
     id: 'a2',
     title: 'ភាសាខ្មែរ ថ្នាក់ទី១',
-    thumbnail: 'https://i.ytimg.com/vi/oQ3S_h8zyvM/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGFsgZChlMA8=&rs=AOn4CLDWJGnXB0WQ5A38yn4CgOcwG0LgPw', // Example thumbnail
+    thumbnail: 'https://i.ytimg.com/vi/oQ3S_h8zyvM/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGFsgZChlMA8=&rs=AOn4CLDWJGnXB0WQ5A38yn4CgOcwG0LgPw', 
     description: '(សៀវភៅជំនួយការអាន)',
     playlistId: 'PLDt349AvcEaIGF4Mp8k-gZVJYFwXyOWxi',
   },
     {
     id: 'a3',
     title: 'វីដេអូអប់រំ',
-    thumbnail: 'https://i.ytimg.com/vi/iI9Qzd2gMpc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGF8gEyh_MA8=&rs=AOn4CLB4kYD1v0hvnaFoIVjGLWI9x3McmQ', // Example thumbnail
+    thumbnail: 'https://i.ytimg.com/vi/iI9Qzd2gMpc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGF8gEyh_MA8=&rs=AOn4CLB4kYD1v0hvnaFoIVjGLWI9x3McmQ', 
     description: 'សៀវភៅជំនួយភាសាខ្មែរថ្នាក់ទី១ ភាគ១',
     playlistId: 'PLDt349AvcEaIp6Zbs_-JAVRPm4DE1u3QN',
   },
     {
     id: 'a4',
     title: 'មូលដ្ឋានគ្រឹះចំណេះដឹងហិរញ្ញវត្ថុ',
-    thumbnail: 'https://i.ytimg.com/vi/vyeeGCUDBd8/maxresdefault.jpg', // Example thumbnail
+    thumbnail: 'https://i.ytimg.com/vi/vyeeGCUDBd8/maxresdefault.jpg', 
     description: 'ហេតុអ្វីបានជាសិស្សានុសិស្សគួរមានចំណេះដឹងមូលដ្ឋានផ្នែកហិរញ្ញវត្ថុ? តើចំណេះដឹងហិរញ្ញវត្ថុមានសារៈសំខាន់បែបណា?',
     playlistId: 'PLDt349AvcEaL8Xgl7HoeUxZpd4lOKpI3a',
   },
     {
     id: 'a5',
-    title: 'ដំណោះស្រាយបច្ចេកវិទ្យាឌីជីថល សម្រាប់លើកកម្ពស់ការអប់រំ បំណិនជីវិតតាមមូលដ្ឋាន',
-    thumbnail: 'https://i.ytimg.com/vi/yxx2-nDxAXc/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgZShlMA8=&rs=AOn4CLBGhZFTWSgDcsbQtspDMcfP9s3nng', // Example thumbnail
-    description: 'ប្រមូលផ្តុំរឿងនិទានអប់រំល្អៗជាច្រើនសម្រាប់កុមារ និងមហាជន។',
-    playlistId: 'PLDt349AvcEaKE6SAspJgOapTu466E-ETf',
+    title: 'ស្តង់ដាសាលាគំរូ',
+    thumbnail: 'https://i.ytimg.com/vi/avlKzWiwYag/hqdefault.jpg', 
+    description: 'វិធីសាស្រ្ដបង្រៀនបែបថ្មី់',
+    playlistId: 'PLDt349AvcEaJ_ho4S0F5aSkjBpwjNJY96',
   },
+      {
+    id: 'a6',
+    title: 'គណិតវិទ្យារីករាយ (ទំពួន)',
+    thumbnail: 'https://i.ytimg.com/vi/yyoViEGppTU/hqdefault.jpg', 
+    description: 'គណិតវិទ្យាថ្នាក់ទី១',
+    playlistId: 'PLDt349AvcEaIxRQqNfklxWqpcreJ-LbNu',
+  },
+      {
+    id: 'a7',
+    title: 'សុខភាពភ្នែក និងការថែទាំ',
+    thumbnail: 'https://i.ytimg.com/vi/Jqk3ywVtuvU/hqdefault.jpg', 
+    description: 'ថ្នាក់ទី១០ មេរៀនអប់រំសុខភាពភ្នែក',
+    playlistId: 'PLDt349AvcEaIU2pheMSEzkwDTJpvwLANm',
+  },
+      {
+    id: 'a8',
+    title: 'ថ្នាក់ទី១០ ជីវវិទ្យា សុខភាពផ្លូវភេទ',
+    thumbnail: 'https://i.ytimg.com/vi/M3fGM5x0JP4/maxresdefault.jpg', 
+    description: 'បង្រៀនដោយ ៖ អ្នកគ្រូ ឡុង គឹមណា',
+    playlistId: 'PLDt349AvcEaIyj2gqLGeAxgy5tL9BIUUL',
+  },
+      {
+    id: 'a9',
+    title: 'គណិតវិទ្យារីករាយ ថ្នាក់ទី២ (ខ្លី)',
+    thumbnail: 'https://i.ytimg.com/vi/cPAOGniV-yg/maxresdefault.jpg', 
+    description: 'សៀវភៅលំហាត់កូនស្វាឆ្លាតន១',
+    playlistId: 'PLDt349AvcEaIR4FEmwruhsKpguz0HBJ_D',
+  },
+      {
+    id: 'a10',
+    title: 'គណិតវិទ្យារីករាយ ថ្នាក់ទី១ (ខ្លី)',
+    thumbnail: 'https://i.ytimg.com/vi/TPyk2t9tjkQ/maxresdefault.jpg', 
+    description: 'សៀវភៅលំហាត់កូនស្វាឆ្លាតគ១',
+    playlistId: 'PLDt349AvcEaJKZpQgTkOh6UAwdnCsCckI',
+  },
+      {
+    id: 'a11',
+    title: 'អក្ខរកម្មប្រព័ន្ធផ្សព្វផ្សាយ និងព័ត៌មាន',
+    thumbnail: 'https://i.ytimg.com/vi/dzO3UGharmg/hqdefault.jpg', 
+    description: 'Media_Information_Literacy #អក្ខរកម្មប្រព័ន្ធផ្សព្វផ្សាយនិងព័ត៌មាន',
+    playlistId: 'PLDt349AvcEaI2hvQeme9SM8HBlszu0v6k',
+  },
+  
+
 ];
 
-// Export the configuration array using CommonJS module system
 module.exports = {
     collectionsConfig
 };
